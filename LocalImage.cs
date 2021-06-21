8a16b4c17d480c6a35d202f3e67d6f0e9644dcff
JaredDC <jared.dc@outlook.com>
2021-06-21 14:14:58 +0800

    1.change name 2.set stderr/stdout to file

--- LocalImage.cs
+++ LocalImage.cs
@@ -2,31 +2,45 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
+using System.Linq;
 
 public class LocalImage
 {
 	private List<string> files = null;
 	private string path;
+	private int invalidCnt = 0;
 	public enum Update : int
 	{
 		YES,
 		NO,
-		FORCE
+		FORCE,
+		CleanInvalid
 	}
 	private Update update = LocalImage.Update.NO;
 	public LocalImage(string path)
 	{
 		this.path = path;
 	}
+	public bool ShoulditUpdate(string path="")
+    {
+		if (path.Length < 1)
+		{
+			path = this.path;
+		}
+
+		Console.WriteLine(new FileInfo(path).LastWriteTime);
+		return true;
+    }
+
 	public void ScanLocalPath(string path="", bool print=true)
     {
 		if (path.Length < 1) {
 			path = this.path;
 		}
 		List<string> files = new List<string>();
-		foreach (string file in System.IO.Directory.GetFiles(path, "*", SearchOption.AllDirectories))
+		foreach (string file in Directory.GetFiles(path, "*", SearchOption.AllDirectories))
 		{
-			long length = new System.IO.FileInfo(file).Length/1024;
+			long length = new FileInfo(file).Length/1024;
 			string file_low = file.ToLower();
 			if (file_low.EndsWith(".jpg") || file_low.EndsWith(".jpeg") || file_low.EndsWith(".png")) { 
 				if (length > 100) {
@@ -44,6 +58,7 @@ public class LocalImage
 			if (print) { Console.WriteLine("No suitable image files."); }
 		}
 		this.files = files;
+		List2Txt();
 	}
 	public void List2Txt(string txtFile="_img_list.txt")
     {
@@ -58,6 +73,15 @@ public class LocalImage
 		try {
 			if (this.update != Update.NO) {
 				File.WriteAllLines(txtFile, this.files);
+				if (this.update == Update.FORCE)
+                {
+					Console.WriteLine("Created: {0}", txtFile);
+				} else if (this.update == Update.CleanInvalid)
+                {
+					Console.WriteLine("Clean Invalid files[{1:D}]: {0}", txtFile, this.invalidCnt);
+				} else { 
+					Console.WriteLine("Updated: {0}", txtFile);
+				}
 				// update config.ini modified time.
 			}
 		} catch (ArgumentNullException e)
@@ -65,4 +89,49 @@ public class LocalImage
 			Console.WriteLine("Exception caught: {0}", e);
 		}
     }
+	public void Txt2List(string txtFile = "_img_list.txt")
+	{
+		txtFile = Path.Combine(this.path, txtFile);
+		if (this.files == null)
+		{
+			this.ScanLocalPath(print: false);
+		}
+		if (!new FileInfo(txtFile).Exists)
+		{
+			ScanLocalPath();
+		}
+		
+		try
+		{
+			List<string> readfiles = File.ReadLines(txtFile).ToList();
+			List<string> effectivefiles = new List<string>();
+			foreach (string file in readfiles)
+			{
+				if (File.Exists(file))
+                {
+					effectivefiles.Add(file);
+				} else
+                {
+					Console.WriteLine("File does not exist: " + file);
+				}
+				
+			}
+			this.invalidCnt = readfiles.Count - effectivefiles.Count;
+			if (this.invalidCnt != 0)
+            {
+				this.update = Update.CleanInvalid;
+				List2Txt();
+			}
+			this.files = effectivefiles;
+		}
+		catch (Exception e)
+		{
+			Console.WriteLine("Exception caught: {0}", e);
+		} finally
+        {
+			/*foreach (string s in this.files) {
+				Console.WriteLine(s);
+			}*/
+        }
+	}
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +11,55 @@ namespace wallpaper_today
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                using (var writer = new StreamWriter("log.txt"))
+                {
+                    Console.SetOut(writer);
+                    Console.SetError(writer);
+
+                    //Console.Error.WriteLine("Error information written to begin");
+                    DailyWallpaper();
+                    //Console.Error.WriteLine("Error information written to TEST");
+                    Console.Out.Close();
+                    Console.Error.Close();
+                    writer.Close();
+                    // Console.SetOut(Console.OpenStandardOutput());
+                }
+            } catch (IOException e)
+            {
+                TextWriter errorWriter = Console.Error;
+                errorWriter.WriteLine(e.Message);
+                return;
+            }
+            // redirect stderr to default.
+            var standardError = new StreamWriter(Console.OpenStandardError());
+            standardError.AutoFlush = true;
+            Console.SetError(standardError);
+
+            // redirect stdout to default.
+            var standardOutput = new StreamWriter(Console.OpenStandardOutput());
+            standardOutput.AutoFlush = true;
+            Console.SetOut(standardOutput);
+            
+            // print the log file.
+            Console.WriteLine(File.ReadAllText("log.txt"));
+
+        }
+
+        private static bool DailyWallpaper()
+        {
             // Wallpaper.SetWallPaper("C:\\Users\\jared\\Pictures\\pic\\61e030ca5106db3731eb1058432c6bbb7b4b5d3efbfe63001fbd56d9ae288daf.jpg");
             Wallpaper.AddWaterMark("C:\\Users\\jared\\Pictures\\photo_of_the_day\\黑沙滩上Reynisdrangar的玄武岩_冰岛_Cavan_Images_Getty_Images_watermark.jpg",
                 "D:\\jared\\coding\\wallpaper_today\\watermark.jpg",
                 "黑沙滩上Reynisdrangar的玄武岩,冰岛@Cavan Images Getty Images");
-            new LocalImage(@"C:\Users\jared\Pictures\新建文件夹").ScanLocalPath();
-            new LocalImage(@"C:\Users\jared\Pictures\新建文件夹").List2Txt();
+            //new LocalImage(@"C:\Users\jared\Pictures\新建文件夹").ScanLocalPath();
+            new LocalImage(@"C:\Users\jared\Pictures\新建文件夹").ShoulditUpdate();
             //new LocalImage("C:\\Users\\jared\\Pictures\\photo_of_the_day").ScanLocalPath();
             //new LocalImage("D:\\jared\\Videos\\movies\\seen").ScanLocalPath();
+            // Creating a text file named "out" in D Drive
+
+            return true;
         }
 
     }
